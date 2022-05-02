a2b5ea7341927c0459bfb00d4b8545f6beaec888
deepali <deepalisonavane5751@gmail.com>
2022-05-02 23:38:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Restaurant from './component/Basics/Restaurant'
+import UseState from './component/Hooks/UseState';
+import UseEffect from './component/Hooks/UseEffect';
+const App = () => {
+  return <Restaurant />;
+
+}
+
+export default App;
